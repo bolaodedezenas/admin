3cb7bbb9a9029b5c1b962b845985a70a6cc81b73
Fernando <piloukos45@gmail.com>
2025-12-02 20:00:03 -0300

    test

--- app/(auth)/recovery/page.jsx
+++ app/(auth)/recovery/page.jsx
@@ -1,15 +1,120 @@
-"use client";
-import { use, useEffect, useState } from 'react';
+// "use client";
+// import { use, useEffect, useState } from 'react';
+// import { useRouter } from 'next/navigation';
+// import { auth } from '@/libs/firebase/FirebaseConfig';
+// import { applyActionCode } from 'firebase/auth';
+// // icon 
+// import Icon from '@/components/Icon';
+
+// import { useAuth } from "@/context/AuthContext";
+// // components
+// import Loading from "@/components/Loading";
+// import ResetPasswordForm from "@/components/Forms/AuthForms/ResetPasswordForm";
+
+// export default function Recovery() {
+//   const { loading, setLoading } = useAuth(); // pega as funções do contexto
+//   const router = useRouter();
+//   const [oobCode, setOobCode] = useState('');
+//   const [mode, setMode] = useState('');
+//   const [close, setClose] = useState('');
+//   const [message, setMessage] = useState('');
+
+//   useEffect(() => {
+//     console.log(close);
+//   }, [oobCode, mode, close]);
+
+//   useEffect(() => {
+//     setLoading(true);
+//     const query = new URLSearchParams(window.location.search);
+//     const code = query.get('oobCode');
+//     const modeType = query.get('mode');
+//     const continueUrl = query.get('continueUrl');
+
+//     // Decodifica a continueUrl (remove os %3F, %2F, etc.)
+//     const decodedUrl = continueUrl ? decodeURIComponent(continueUrl) : '';
+//     // Captura o número que vem depois de ?
+//     const numMatch = decodedUrl.match(/\?(\d+)/);
+//     const pageCode = numMatch ? numMatch[1] : null;
+
+//     setOobCode(code);
+//     setMode(modeType);
+//     setClose(pageCode);
+
+//     if (!code) return router.replace('/not-found');
+
+//     // Se for verificação de email
+//     if (modeType === 'verifyEmail' || modeType === 'verifyAndChangeEmail') {
+//       applyActionCode(auth, code)
+//         .then(() => {
+//           setMessage({
+//             title: 'Email verificado com sucesso!',
+//             text: 'Agora vocé pode fazer login no app.',
+//           });
+//         })
+//         .catch((error) => {
+//           console.error(error);
+//           setMessage({
+//             title: 'Erro ao verificar email!',
+//             text: 'Link expirado ou inválido, solicite um novo link de verificação.',
+//           });
+//         });
+//       setTimeout(() => router.replace(`/login`), 10000);
+//     }
+//     setLoading(false);
+//   }, []);
+
+//   if (loading) return <Loading />;
+
+//   return (
+//     <div
+//       className='
+//       min-h-screen 
+//       flex items-center justify-center flex-col
+//       bg-gradient-to-t from-[rgb(var(--background-secundary))] to-[rgb(var(--background))]
+//       p-4
+//       '
+//     >
+//       {mode === 'resetPassword' ? (
+//         <ResetPasswordForm oobCode={oobCode} />
+//       ) : (
+//         <div className=' p-5 w-full max-w-[650px] text-center  '>
+//           {message.title === 'Erro ao verificar email!' ? (
+//             <Icon name='Warning' size={100} color='red' />
+//           ) : (
+//             <Icon name='Verified_User' size={100} color='white' />
+//           )}
+//           <h3 className='text-[rgb(var(--white))] text-[2.8rem] '>
+//             {message.title}
+//           </h3>
+//           <p className='text-[rgb(var(--white))] text-[1.3rem] '>
+//             {message.text}
+//           </p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+
+
+
+
+'use client';
+
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { auth } from '@/libs/firebase/FirebaseConfig';
+import { auth, db } from '@/libs/firebase/FirebaseConfig';
 import { applyActionCode } from 'firebase/auth';
-// icon 
+import { doc, updateDoc } from 'firebase/firestore';
+
+// icon
 import Icon from '@/components/Icon';
 
-import { useAuth } from "@/context/AuthContext";
+// context
+import { useAuth } from '@/context/AuthContext';
 // components
-import Loading from "@/components/Loading";
-import ResetPasswordForm from "@/components/Forms/AuthForms/ResetPasswordForm";
+import Loading from '@/components/Loading';
+import ResetPasswordForm from '@/components/Forms/AuthForms/ResetPasswordForm';
 
 export default function Recovery() {
   const { loading, setLoading } = useAuth(); // pega as funções do contexto
@@ -24,7 +129,6 @@ export default function Recovery() {
   }, [oobCode, mode, close]);
 
   useEffect(() => {
-    setLoading(true);
     const query = new URLSearchParams(window.location.search);
     const code = query.get('oobCode');
     const modeType = query.get('mode');
@@ -42,26 +146,41 @@ export default function Recovery() {
 
     if (!code) return router.replace('/not-found');
 
-    // Se for verificação de email
-    if (modeType === 'verifyEmail' || modeType === 'verifyAndChangeEmail') {
-      applyActionCode(auth, code)
-        .then(() => {
-          setMessage({
-            title: 'Email verificado com sucesso!',
-            text: 'Agora vocé pode fazer login no app.',
-          });
-        })
-        .catch((error) => {
-          console.error(error);
-          setMessage({
-            title: 'Erro ao verificar email!',
-            text: 'Link expirado ou inválido, solicite um novo link de verificação.',
-          });
+    const verifyEmail = async () => {
+      setLoading(true);
+      try {
+        await applyActionCode(auth, code); // aplica o código
+        await auth.currentUser.reload(); // atualiza o usuário
+
+        // Atualiza o email no Firestore
+        const userDocRef = doc(db, 'users', auth.currentUser.uid);
+        await updateDoc(userDocRef, {
+          email: auth.currentUser.email,
         });
-      setTimeout(() => router.replace(`/login`), 10000);
+
+        setMessage({
+          title: 'Email verificado com sucesso!',
+          text: 'Seu email foi confirmado e seus dados no perfil foram atualizados.',
+        });
+
+        setTimeout(() => router.replace(`/login`), 8000);
+      } catch (error) {
+        console.error(error);
+        setMessage({
+          title: 'Erro ao verificar email!',
+          text: 'Link expirado ou inválido, solicite um novo link de verificação.',
+        });
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (modeType === 'verifyEmail' || modeType === 'verifyAndChangeEmail') {
+      verifyEmail();
+    } else {
+      setLoading(false);
     }
-    setLoading(false);
-  }, []);
+  }, [router, setLoading]);
 
   if (loading) return <Loading />;
 
@@ -77,16 +196,16 @@ export default function Recovery() {
       {mode === 'resetPassword' ? (
         <ResetPasswordForm oobCode={oobCode} />
       ) : (
-        <div className=' p-5 w-full max-w-[650px] text-center  '>
+        <div className='p-5 w-full max-w-[650px] text-center'>
           {message.title === 'Erro ao verificar email!' ? (
             <Icon name='Warning' size={100} color='red' />
           ) : (
             <Icon name='Verified_User' size={100} color='white' />
           )}
-          <h3 className='text-[rgb(var(--white))] text-[2.8rem] '>
+          <h3 className='text-[rgb(var(--white))] text-[2.8rem]'>
             {message.title}
           </h3>
-          <p className='text-[rgb(var(--white))] text-[1.3rem] '>
+          <p className='text-[rgb(var(--white))] text-[1.3rem]'>
             {message.text}
           </p>
         </div>
@@ -94,3 +213,5 @@ export default function Recovery() {
     </div>
   );
 }
+
+
